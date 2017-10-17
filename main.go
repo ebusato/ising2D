b2b725ed8b6d43b1544d5225864ebc6fa6965fb2
Busato <emmanuel.busato@gmail.com>
2017-10-17 15:22:49 +0200

    update

--- main.go
+++ main.go
@@ -20,6 +20,86 @@ import (
 	"go-hep.org/x/hep/hplot"
 )
 
+type Spin struct {
+	Val int // -1 or +1
+}
+
+type Grid struct {
+	M [][]Spin
+	N int     // number of rows and columns (they are both equal to the same value: N)
+	J float64 // coupling
+}
+
+func NewGrid(n int, j float64) *Grid {
+	G := &Grid{}
+	G.M = make([][]Spin, n)
+	for i, _ := range G.M {
+		G.M[i] = make([]Spin, n)
+	}
+	G.N = n
+	G.N = n
+	G.J = j
+	return G
+}
+
+func (g *Grid) Init() {
+	for i := range g.M {
+		for j := range g.M[i] {
+			s := Spin{}
+			rand := rand.Float64()
+			if rand < 0.5 {
+				s.Val = -1
+			} else {
+				s.Val = +1
+			}
+			g.M[i][j] = s
+		}
+	}
+}
+
+func (g *Grid) Plot() {
+	h2d := hbook.NewH2D(g.N, 0, float64(g.N), g.N, 0, float64(g.N))
+	for i := range g.M {
+		for j := range g.M[i] {
+			s := g.M[i][j]
+			if s.Val == 1 {
+				h2d.Fill(float64(i), float64(j), 1)
+			}
+		}
+	}
+	plotH2D(h2d)
+}
+
+func (g *Grid) FlipRandomSpin() {
+	i := rand.Intn(g.N)
+	j := rand.Intn(g.N)
+	g.M[i][j].Val *= -1
+}
+
+/*
+func (g *Grid) Energy() {
+	for i := range g.M {
+		for j := range g.M[i] {
+
+		}
+	}
+}
+*/
+
+func main() {
+	flag.Parse()
+
+	go webServer()
+
+	grid := NewGrid(10, 1)
+	grid.Init()
+	for {
+		time.Sleep(100 * time.Millisecond)
+		grid.Plot()
+		grid.FlipRandomSpin()
+	}
+}
+
 var (
 	addrFlag = flag.String("addr", ":5555", "server address:port")
 	datac    = make(chan plots)
@@ -35,26 +115,6 @@ func webServer() {
 	err := http.ListenAndServe(*addrFlag, nil)
 	if err != nil {
 		panic(err)
-	}
-}
-
-func main() {
-	flag.Parse()
-
-	go webServer()
-	generate()
-}
-
-func generate() {
-	for {
-		time.Sleep(100 * time.Millisecond)
-		randX := rand.Float64()
-		randY := rand.Float64()
-		fmt.Println(randX)
-
-		h2d := hbook.NewH2D(10, 0, 10, 10, 0, 10)
-		h2d.Fill(10*randX, 10*randY, 1)
-		plotH2D(h2d)
 	}
 }
 
